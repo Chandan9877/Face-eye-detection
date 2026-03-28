e1f9a15920b5c47ca0445e2cf638024a9480c630
chandan9877 <chandanmahato.com@gmail.com>
2026-03-28 08:58:10 +0530

    aadded face_data.py

--- face_recognition/face_data.py
+++ face_recognition/face_data.py
@@ -1,6 +1,11 @@
 import cv2
 import os
 
+path = 'dataset'
+if not os.path.exists(path):
+    os.makedirs(path)
+    print(f"Directory '{path}' created!")
+    
 cam = cv2.VideoCapture(0)
 cam.set(3,640)
 cam.set(4,480)
@@ -24,7 +29,7 @@ while True:
         cv2.rectangle(img,(x,y),(x + w , y + h),(255,0,0),2)
         count += 1
         cv2.imwrite("dataset/user."+str(face_id) + "." + str(count)+".jpg",gray[y:y+h,x:x+w])
-        
+        print("image captured"+str(count))
     cv2.imshow("image",img)
 
     k = cv2.waitKey(100) & 0xff
